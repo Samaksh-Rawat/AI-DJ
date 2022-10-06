04961fe6bd1d9c6a436194776bff71a2d3f21bc5
Samaksh-Rawat <97016925+Samaksh-Rawat@users.noreply.github.com>
2022-10-06 17:29:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function setup()
 {
@@ -7,7 +11,27 @@ function setup()
 
     video=createCapture(VIDEO)
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);    
 }
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+        righttWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+         
+    }
+}
+function modelLoaded()
+    {
+        console.log('PoseNet is initialized');
+    }
 function draw()
 {
     image(video,0,0,600,500);
@@ -19,4 +43,6 @@ function preload()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
